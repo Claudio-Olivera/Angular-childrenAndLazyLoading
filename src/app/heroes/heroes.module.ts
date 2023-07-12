7f26010570c93b3a1511277a218b1c4c8e37d65e
Claudio-Olivera <claudioolivera753@gmail.com>
2023-07-12 19:18:19 -0300

    end section 14

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../material/material.module';
 
 
@@ -29,6 +30,7 @@ import { HeroImagePipe } from './pipes/hero-image.pipe';
   imports: [
     CommonModule,
     HeroesRoutingModule,
+    ReactiveFormsModule,
 
     MaterialModule
   ]
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -21,4 +21,10 @@ export class HeroesService {
         catchError( error => of(undefined) )
       )
   }
+
+  getSuggestions(query:string):Observable<Hero[]>{
+    return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${query}&_limit=6`);
+  }
+
+
 }
